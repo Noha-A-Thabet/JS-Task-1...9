1234c3de2f6f0a66e3eb617c030f2357f52c6f66
Noha-A-Thabet <NohaAshraf25@outlook.com>
2022-05-06 06:23:42 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,45 @@
+// task 2 
+document.write("<h1>Elzero</h1>");
+document.querySelector("h1").style.color = "blue";
+document.querySelector("h1").style.fontSize = "80px";
+document.querySelector("h1").style.fontWeight = "bold";
+document.querySelector("h1").style.textAlign = " center";
+document.querySelector("h1").style.fontFamily = "Arial";
+
+
+
+
+// Task 3 
+
+console.log("%celzero %cweb %cschool", "color:red;font-size:40px", "color:green;font-size:40px", "background-color:blue;color:white;font-weight:bold;font-size:40px")
+
+// Task 4
+console.group("Group 1 ");
+console.log("Message One");
+console.log("Message Two");
+console.group("Child Group");
+console.log("Message One");
+console.log("Message Two");
+console.group("Grand Child Group");
+console.log("Message One");
+console.log("Message Two");
+console.groupEnd();
+console.groupEnd();
+console.groupEnd();
+console.group("Group 2 ");
+console.log("Message One");
+console.log("Message Two");
+console.groupEnd();
+
+// task 5 
+console.table(["Elzero", "Ahmed", "Sameh", "Gamal", "Aya"])
+
+
+// TASK 6
+/*
+console.log("Iam In Console");
+document.write("Iam In Page");
+*/
+
+// console.log("Iam In Console");
+// document.write("Iam In Page");
